8d1824eabaee5fcd6619f319218bdd0991831f76
nikhilserialcom <1nikhil.serialcominfotech@gmail.com>
2023-09-06 18:22:17 +0530

    add app js file

--- public/vendor/wink/app.js
+++ public/vendor/wink/app.js
@@ -24488,25 +24488,7 @@
 					attrs: {
 						d: "M0 4c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm11 9l-3-3-6 6h16l-5-5-2 2zm4-4a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
 					}
-				})])]), 
-				t._v(" "), n("button", {
-					on: {
-						click:function(e) {
-							t.openingHTMLEmbedder()
-						}
-					}
-				}, [n("svg", {
-					staticClass: "fill-current w-3",
-					attrs: {
-						xmlns: "http://www.w3.org/2000/svg",
-						viewBox: "0 0 20 20"
-					}
-				}, [n("path", {
-					attrs: {
-						d: "M 74.9 21.4 H 25.1 c -8.4 0 -15.1 6.8 -15.1 15.1 v 26.9 c 0 8.4 6.8 15.1 15.1 15.1 h 49.8 c 8.4 0 15.1 -6.8 15.1 -15.1 V 36.5 c 0 -8.3 -6.8 -15.1 -15.1 -15.1 Z M 39.5 62.3 V 37.7 l 21 12.3 Z",
-					},
-				})])])
-				,t._v(" "), n("button", {
+				})])]),t._v(" "), n("button", {
 					on: {
 						click: function(e) {
 							t.openingHTMLEmbedder()
